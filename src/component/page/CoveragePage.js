f67d4e38a06354eaff1e9d8bc8bf3b6dbe5feda6
Kittipat Poosrimoung <first1801@hotmail.com>
2021-06-04 22:30:13 +0700

    Add bottom at CoveragePage

--- src/component/page/CoveragePage.js
+++ src/component/page/CoveragePage.js
@@ -3,6 +3,7 @@ import PageHeader from "./PageHeader";
 import pageDetail from "../../pageDetail";
 import Board from "../Board";
 import CoverageBoard from "../CoverageBoard";
+import StyledButton from "../StyledButton";
 export default function CoveragePage(props) {
   return (
     <PageHeader
@@ -14,6 +15,30 @@ export default function CoveragePage(props) {
       <Board>
         <CoverageBoard />
       </Board>
+      <div className="pageHeader" style={{ marginTop: "1em" }}>
+        <div>
+          <StyledButton
+            variant="contained"
+            style={{ background: "var(--primary)" }}
+          >
+            บันทึก
+          </StyledButton>
+          <StyledButton variant="contained" color="secondary">
+            ลบข้อมูล
+          </StyledButton>
+        </div>
+        <div className="flexend">
+          <StyledButton variant="contained" style={{ background: "#808080" }}>
+            ย้อนกลับ
+          </StyledButton>
+          <StyledButton
+            variant="contained"
+            style={{ background: "var(--primary)" }}
+          >
+            ถัดไป
+          </StyledButton>
+        </div>
+      </div>
     </PageHeader>
   );
 }
