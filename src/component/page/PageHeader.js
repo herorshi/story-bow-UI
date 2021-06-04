c96672ca30eae3daaae78b2a2e02b5a97304d566
Kittipat Poosrimoung <first1801@hotmail.com>
2021-06-04 14:58:17 +0700

    PageHeader use props function

--- src/component/page/PageHeader.js
+++ src/component/page/PageHeader.js
@@ -16,7 +16,13 @@ export default function PageHeader(props) {
         <h1>{props.data.title}</h1>
         <h1>
           {props.fn0 && (
-            <StyledButton variant="contained" style={{ background: "#808080" }}>
+            <StyledButton
+              variant="contained"
+              style={{ background: "#808080" }}
+              onClick={() => {
+                props.fn0();
+              }}
+            >
               {props.fn0txt}
             </StyledButton>
           )}
@@ -24,12 +30,24 @@ export default function PageHeader(props) {
             รับชมวีดีโอสอน
           </StyledButton>
           {props.fn2 && (
-            <StyledButton variant="contained" style={{ background: "#FFA500" }}>
+            <StyledButton
+              variant="contained"
+              style={{ background: "#FFA500" }}
+              onClick={() => {
+                props.fn2();
+              }}
+            >
               {props.fn2txt}
             </StyledButton>
           )}
           {props.fn3 && (
-            <StyledButton variant="contained" color="primary">
+            <StyledButton
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                props.fn3();
+              }}
+            >
               {props.fn3txt}
             </StyledButton>
           )}
