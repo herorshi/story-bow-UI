6f52d50df6b54d9f2be4c5ea7a61efa48184751f
Kittipat Poosrimoung <first1801@hotmail.com>
2021-06-05 14:30:34 +0700

    create ModalEditOrTool from ModalEditTxt

---
+++ src/component/ModalEditOrTool.js
@@ -0,0 +1,40 @@
+import react from "react";
+import StyledButton from "./StyledButton";
+import PageHeader from "./page/PageHeader";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import CssTextField from "./CssTextField";
+export default function ModalEditOrTool({
+  InputTitle,
+  setTxt,
+  edittxt,
+  handleClose,
+  handleChange,
+}) {
+  return (
+    <PageHeader data={InputTitle}>
+      <CssTextField edittxt={edittxt} handleChange={handleChange}/>
+
+      <div className="flexend" style={{ marginTop: "1em" }}>
+        <StyledButton
+          variant="contained"
+          style={{ background: "#808080", width: "10em" }}
+          onClick={() => {
+            handleClose();
+          }}
+        >
+          ปิด
+        </StyledButton>
+        <StyledButton
+          variant="contained"
+          style={{ background: "var(--primary)" }}
+          onClick={() => {
+            setTxt(edittxt);
+            handleClose();
+          }}
+        >
+          ยืนยันรายการ
+        </StyledButton>
+      </div>
+    </PageHeader>
+  );
+}
